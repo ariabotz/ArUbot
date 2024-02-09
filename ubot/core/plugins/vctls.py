81099bf936fe0531587bbebea330e7610c435258
ariabotz <157705941+ariabotz@users.noreply.github.com>
2024-02-09 21:05:24 +0700

    Update vctls.py

--- ubot/core/plugins/vctls.py
+++ ubot/core/plugins/vctls.py
@@ -1,3 +1,5 @@
+
+ 
 from asyncio import sleep
 from contextlib import suppress
 from random import randint
@@ -9,7 +11,6 @@ from pyrogram.raw.functions.messages import GetFullChat
 from pyrogram.raw.functions.phone import CreateGroupCall, DiscardGroupCall
 from pyrogram.raw.types import InputGroupCall, InputPeerChannel, InputPeerChat
 from pyrogram.types import Message
-
 from ubot import *
 
 
@@ -27,60 +28,43 @@ async def get_group_call(
             ).full_chat
         if full_chat is not None:
             return full_chat.call
-    await eor(message, f"ɴᴏ ɢʀᴏᴜᴘ ᴄᴀʟʟ ꜰᴏᴜɴᴅ {err_msg}")
+    await eor(message, f"**No group call Found** {err_msg}")
     return False
 
 
 async def start_vctools(client, message):
-    flags = " ".join(message.command[1:])
-    ky = await message.reply("<code>ᴍᴇᴍᴘʀᴏꜱᴇꜱ....</code>")
-    vctitle = get_arg(message)
-    if flags == enums.ChatType.CHANNEL:
-        chat_id = message.chat.title
-    else:
-        chat_id = message.chat.id
-    args = (
-        f"<b>ᴏʙʀᴏʟᴀɴ ꜱᴜᴀʀᴀ ᴀᴋᴛɪꜰ</b>\n<b>ᴄʜᴀᴛ : </b><code>{message.chat.title}</code>"
-    )
+    ky = await message.reply("<code>Processing....</code>")
+    chat_id = message.chat.id
     try:
-        if not vctitle:
-            await client.invoke(
-                CreateGroupCall(
-                    peer=(await client.resolve_peer(chat_id)),
-                    random_id=randint(10000, 999999999),
-                )
+        await client.invoke(
+            CreateGroupCall(
+            peer=(await client.resolve_peer(chat_id)),
+            random_id=randint(0, 2147483647),
             )
-        else:
-            args += f"\n<b>ᴛɪᴛʟᴇ : </b> <code>{vctitle}</code>"
-            await client.invoke(
-                CreateGroupCall(
-                    peer=(await client.resolve_peer(chat_id)),
-                    random_id=randint(10000, 999999999),
-                    title=vctitle,
-                )
-            )
-        await ky.edit(args)
+        )
+        await ky.edit(f"<b>• Obrolan Suara Aktif</b>\n<b>• Chat : </b><code>{message.chat.title}</code>")
     except Exception as e:
-        await ky.edit(f"<b>INFO:</b> `{e}`")
+        await ky.edit(f"<b>Error:</b> <code{e}</code>")
 
 
 
 async def stop_vctools(client, message):
-    ky = await message.reply("<code>ᴍᴇᴍᴘʀᴏꜱᴇꜱ....</code>")
+    ky = await message.reply("<code>Processing....</code>")
     message.chat.id
     if not (
-        group_call := (await get_group_call(client, message, err_msg=", ᴋᴇꜱᴀʟᴀʜᴀɴ..."))
+        group_call := (await get_group_call(client, message, err_msg=", Kesalahan..."))
     ):
         return
     await client.invoke(DiscardGroupCall(call=group_call))
     await ky.edit(
-        f"<b>ᴏʙʀᴏʟᴀɴ ꜱᴜᴀʀᴀ ᴅɪᴀᴋʜɪʀɪ</b>\n<b>ᴄʜᴀᴛ : </b><code>{message.chat.title}</code>"
+        f"<b>• Obrolan Suara Diakhiri</b>\n<b>• Chat : </b><code>{message.chat.title}</code>"
     )
+
 
 
 async def join_os(client, message):
     kk = message.from_user.id
-    ky = await message.reply("<code>ᴍᴇᴍᴘʀᴏꜱᴇꜱ....</code>")
+    ky = await message.reply("<code>Processing....</code>")
     chat_id = message.command[1] if len(message.command) > 1 else message.chat.id
     with suppress(ValueError):
         chat_id = int(chat_id)
@@ -90,14 +74,13 @@ async def join_os(client, message):
     except Exception as e:
         return await ky.edit(f"ERROR: {e}")
     await ky.edit(
-        f"<b>ʙᴇʀʜᴀꜱɪʟ ᴊᴏɪɴ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ</b>\n<b>ᴄʜᴀᴛ : </b><code>{message.chat.title}</code>"
+        f"• <b>Berhasil Join Voice Chat</b>\n<b>• Chat : </b><code>{message.chat.title}</code>"
     )
     await client.vc.set_is_mute(True)
 
 
-
 async def turun_os(client, message):
-    ky = await message.reply("<code>ᴍᴇᴍᴘʀᴏꜱᴇꜱ....</code>")
+    ky = await message.reply("<code>Processing....</code>")
     chat_id = message.command[1] if len(message.command) > 1 else message.chat.id
     with suppress(ValueError):
         chat_id = int(chat_id)
@@ -107,7 +90,7 @@ async def turun_os(client, message):
 
     except Exception as e:
         return await ky.edit(f"<b>ERROR:</b> {e}")
-    msg = "<b>ʙᴇʀʜᴀꜱɪʟ ᴍᴇɴɪɴɢɢᴀʟᴋᴀɴ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ</b>\n"
+    msg = "• <b>Berhasil Meninggalkan Voice Chat</b>\n"
     if chat_id:
-        msg += f"<b>ᴄʜᴀᴛ : </b><code>{message.chat.title}</code>"
+        msg += f"<b>• Chat : </b><code>{message.chat.title}</code>"
     await ky.edit(msg)
